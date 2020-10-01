b9b8ed4fe1ab217e3e4d0543c06fe7f6f4c82c5e
blake <ekalb81@gmail.com>
2020-10-01 07:03:44 -0400

    added axios, call to postman echo for api test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+const axios = require("axios");
 
 const hangOn = () =>
   new Promise(function (resolve) {
@@ -8,7 +9,9 @@ const hangOn = () =>
 (async () => {
   console.time();
   for (let i = 0; i < 3; i++) {
-    console.log(await hangOn());
+    console.log(i);
+    const res = await axios.get("https://postman-echo.com/delay/5");
+    console.log(res.status, res.data);
     console.timeLog();
   }
   console.timeLog();
